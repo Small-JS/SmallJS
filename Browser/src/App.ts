9b5ae077e242f5fe907670525b918f2d49177532
FunctionPoint <FunctionPoint@users.noreply.github.com>
2024-08-04 23:14:51 +0200

    Browser: Added Canvas support #15 partial
    Implemented unit tests where possible.
    Drawing methods will be tested visually later.
    Implemented classes for DOM point and matrix operations.
    Moved component classes to separate folder.
    Fixed debugging source-mapped ST files with Firefox.
    (Is still a todo for other browser projects)
    Updated NPM packages.

--- Browser/src/App.ts
+++ Browser/src/App.ts
@@ -1,5 +1,5 @@
 // Invoke the start method on a new BrowserApp ST object.
 
-let moduleName: string = "./Smalltalk/BrowserApp.js";
+let moduleName: string = "./Script/BrowserApp.js";
 import( moduleName )
 	.then( module => { module.stMyBrowserApp$class.$new().$start(); } );
