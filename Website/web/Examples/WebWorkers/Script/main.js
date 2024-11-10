1d3083a64b93ee56e949c46c070ffb54a85da45e
FunctionPoint <FunctionPoint@users.noreply.github.com>
2024-11-10 15:14:50 +0100

    Website: Example WebWorkers: Updated .gitignore
    And removed erroneously committed files.

--- Website/web/Examples/WebWorkers/Script/main.js
+++
@@ -1,5 +0,0 @@
-// @ts-ignore: This JS file does not exist yet at TS complile time
-import { stWebWorkersApp$class } from "./WebWorkersApp.js";
-// Invoke the start method on a new WebWorkersApp ST object.
-stWebWorkersApp$class.$new().$start();
-//# sourceMappingURL=main.js.map
--- Website/web/Examples/WebWorkers/Script/worker.js
+++
@@ -1,8 +0,0 @@
-// Must statically import ES modules in worker script.
-// This import does not exist yet for TS, but it will exist for JS.
-// So let TS ignore the error:
-// @ts-ignore
-import { stMyWorker$class } from "./MyWorker.js";
-// Invoke the start method on a new MyWorker ST object.
-stMyWorker$class.$new().$start();
-//# sourceMappingURL=worker.js.map
