9c06bae77666cb0160fee33c58453fde43873ef2
FunctionPoint <FunctionPoint@users.noreply.github.com>
2025-02-09 18:45:40 +0100

    Examples: NodeGui: Skip MacOS ARM machines in instal and build scripts
    Because there is no ARM binary for NodeGui at the moment.

--- Examples/NodeGui/build.sh
+++ Examples/NodeGui/build.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
-# This script builds the Shop Server project.
+# This script builds the NodeGui project.
 
 # Exit script if a step fails
 set -e
 # Set working directory to script directory
 cd "$(dirname "$0")"
+
+# Check for MacOs on ARM, for which there is currently no precompiled NodeGui binary.
+
+if
+	test `uname` = "Darwin" && test `uname -m` != "x86_64"
+then
+	echo "NodeGui MacOS ARM support is currently limited (2025-02-08), exiting."
+	exit 0;
+fi
 
 echo "==== Example NodeGui"
 
--- Examples/NodeGui/install.sh
+++ Examples/NodeGui/install.sh
@@ -6,5 +6,14 @@ set -e
 # Set working directory to script directory
 cd "$(dirname "$0")"
 
+# Check for MacOs on ARM, for which there is currently no precompiled NodeGui binary.
+
+if
+	test `uname` = "Darwin" && test `uname -m` != "x86_64"
+then
+	echo "NodeGui MacOS ARM support is currently limited (2025-02-08), exiting."
+	exit 0;
+fi
+
 echo "Installing NPM packages for example NodeGui"
 npm install
