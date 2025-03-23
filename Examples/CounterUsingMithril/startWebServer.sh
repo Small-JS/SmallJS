d9de10e466373d146d5ec51d1da91eb6203be38b
Paul D. Fernhout <pdfernhout@kurtz-fernhout.com>
2025-03-22 22:09:48 -0400

    Examples: Add CounterUsingMithril example based on Counter

---
+++ Examples/CounterUsingMithril/startWebServer.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Start node static web server.
+
+# Exit script if a step fails
+set -e
+# Set working directory to script directory
+cd "$(dirname "$0")"
+
+echo -n "Directory: "
+pwd
+
+npx http-server web -c-1 -p 3000
