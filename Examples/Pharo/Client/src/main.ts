4f176f4be8ae69084b0826598a2323aad21559a8
FunctionPoint <FunctionPoint@users.noreply.github.com>
2025-04-28 21:31:49 +0200

    Examples: Pharo: Fixed app startup unreliability #35
    By in the browser waiting waiting for a page load event before starting the ST app.

--- Examples/Pharo/Client/src/main.ts
+++ Examples/Pharo/Client/src/main.ts
@@ -1,5 +1,7 @@
 // Invoke the start method on a new PharoClientApp ST object.
 
+// Note: Starting the ST app *after* onload is mandatory for the Pharo Zinc web server
+
 let moduleName: string = "./PharoClientApp.js";
 import( moduleName )
-	.then( module => { module.stPharoClientApp$class.$new().$start() } );
+	.then( module => { window.onload = module.stPharoClientApp$class.$new().$start() } );
