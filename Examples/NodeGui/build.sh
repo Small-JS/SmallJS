bc3dc2073c601b21bca07cdb940965e477f9ef7a
FunctionPoint <FunctionPoint@users.noreply.github.com>
2025-09-08 21:29:57 +0200

    Examples: NodeGui: Re-enabled build for MacOS on ARM silicon #60
    The npm package build for it was fixed in by NodeGui maintainers, tnx!

--- Examples/NodeGui/build.sh
+++ Examples/NodeGui/build.sh
@@ -6,15 +6,6 @@ set -e
 # Set working directory to script directory
 cd "$(dirname "$0")"
 
-# Check for MacOs on ARM, for which there is currently no precompiled NodeGui binary.
-
-if
-	test `uname` = "Darwin" && test `uname -m` != "x86_64"
-then
-	echo "NodeGui MacOS ARM support is currently limited (2025-02-08), exiting."
-	exit 0;
-fi
-
 echo "==== Example NodeGui"
 
 # Compile TypeScript
